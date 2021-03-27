a35e55a6df774a8d0a65b2a9533dea0220d43293
oothkoo <contact@oothkoo.com>
2021-03-27 01:18:42 +0100

    Remove css loaders

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -47,15 +47,7 @@ export default {
       {
         test: /\.json$/,
         use: 'json-loader',
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+      }
     ],
   },
 
